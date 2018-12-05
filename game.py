9ced0eac6f8a041378c80878f46e0f0d09539006
robert neuzil <robertneuzil@gmail.com>
2018-12-05 02:11:52 -0500

    new write

--- game.py
+++ game.py
@@ -11,63 +11,23 @@ https://www.pygame.org/docs/tut/ImportInit.html
 '''
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+WIDTH = 800
+HEIGHT = 600
 
-pygame.display.set_caption('First Game')
+display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Racing Game')
+clock = pygame.time.Clock()
 
-screenwidth = 500
-x = 50
-y = 425
-width = 40
-height = 60
-velocity = 5
-jumpCount = 10
+crashed = False
 
-
-run = True
-isJump = False
-
-while run:
-	
-	pygame.time.delay(100)
-
-	for event in pygame.event.get(): # loops through all event to check if they happened
+while not crashed:
+	for event in pygame.event.get():  # creates a list of events for all that happened
 		if event.type == pygame.QUIT:
-			run = False
+			crashed = True 
 
-	keys = pygame.key.get_pressed()
 
-	if keys[pygame.K_LEFT] and x > velocity:
-		x -= velocity
-
-	if keys[pygame.K_RIGHT] and x < screenwidth - width - velocity:
-		x += velocity
-
-	if not (isJump):
-
-		if keys[pygame.K_UP] and y > velocity :
-			y -= velocity
-
-		if keys[pygame.K_DOWN] and y < screenwidth - height - velocity:
-			y += velocity
-
-		if keys[pygame.K_SPACE]:
-			isJump = True
 	
-	else:
-		if jumpCount >= -10:
-			neg = 1
-			if jumpCount < 0:
-				neg = -1
-			y -= (jumpCount ** 2) * 0.5 * neg
-			jumpCount -= 1
-
-		else:
-			isJump = False
-			jumpCount = 10
-
-	win.fill((0))
-	pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
 	pygame.display.update()
+	clock.tick(60)
 
 pygame.quit()
