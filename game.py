f2992c59d27d905304cc0c8ebb4811c8261f775e
robert neuzil <robertneuzil@gmail.com>
2018-12-05 00:30:05 -0500

    red block jumps

--- game.py
+++ game.py
@@ -21,8 +21,12 @@ y = 425
 width = 40
 height = 60
 velocity = 5
+jumpCount = 10
+
 
 run = True
+isJump = False
+
 while run:
 	
 	pygame.time.delay(100)
@@ -39,12 +43,29 @@ while run:
 	if keys[pygame.K_RIGHT] and x < screenwidth - width - velocity:
 		x += velocity
 
-	if keys[pygame.K_UP] and y > velocity :
-		y -= velocity
+	if not (isJump):
 
-	if keys[pygame.K_DOWN] and y < screenwidth - height - velocity:
-		y += velocity
+		if keys[pygame.K_UP] and y > velocity :
+			y -= velocity
+
+		if keys[pygame.K_DOWN] and y < screenwidth - height - velocity:
+			y += velocity
+
+		if keys[pygame.K_SPACE]:
+			isJump = True
 	
+	else:
+		if jumpCount >= -10:
+			neg = 1
+			if jumpCount < 0:
+				neg = -1
+			y -= (jumpCount ** 2) * 0.5 * neg
+			jumpCount -= 1
+
+		else:
+			isJump = False
+			jumpCount = 10
+
 	win.fill((0))
 	pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
 	pygame.display.update()
