5c9d49777c8965c748275fc8d8a77cecfff32041
robert neuzil <robertneuzil@gmail.com>
2018-12-06 23:24:42 -0500

    car moves left and right now

--- game.py
+++ game.py
@@ -21,19 +21,29 @@ def car(x, y):
 
 x = (width * 0.45)
 y = (height * 0.8)
-
+x_change = 0
 
 while not crashed:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			crashed = True
+		if event.type == pygame.KEYDOWN: # not the down arror key, if you're pressing down ANY key
+			if event.key == pygame.K_LEFT:
+				x_change = -5
+			if event.key == pygame.K_RIGHT:
+				x_change = 5
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				x_change = 0 	
+	x += x_change
 
+	
 	GameDisplay.fill(white)
 	car(x, y)
 	pygame.display.update()
 
-	clock.tick(60)
+	clock.tick(70)
 
 
 pygame.quit()
