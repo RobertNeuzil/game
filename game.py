2f692767c5ed8f82190b5e7e88341110d5bebd13
robert neuzil <robertneuzil@gmail.com>
2018-12-06 21:56:48 -0500

    racing

--- game.py
+++ game.py
@@ -1,101 +1,39 @@
 import pygame
-import sys
-pygame.init() # necessary. Docs say you can call individual and initizia individual modules as well. Like font.
+pygame.init()
 
 
-
-'''
-A list containing the sprites
-'''
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
-bg = pygame.image.load('bg.jpg')
-char = pygame.image.load('standing.png') 
-
+width = 800 
+height = 600
+GameDisplay = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
+crashed = False
+CarImg = pygame.image.load('racecar.png')
 
-x = 50
-y = 400
-velocity = 5 # velocity will be subtracted/added/multiplied/divided from character to make it move places on screen
+black = (0,0,0)
+white = (255,255,255)
+red = (255, 0, 0)
 
-SURF_WIDTH = 500     # easier to change later if in variables
-SURF_HEIGHT = 480	# easier to change later if in variables
-
-CHAR_WIDTH = 64
-CHAR_HEIGHT = 64
-
-RED_RGB = (255, 0, 0)
-BLACK_RGB = (0, 0, 0)
-
-isJump = False
-jumpCount = 10
-left = False
-right = False
-walkCount = 0
-
-window = pygame.display.set_mode((SURF_WIDTH, SURF_HEIGHT)) #sets the size of "window"/ surface
-pygame.display.set_caption("First Game") # appears at top of box
-
-def redrawGameWindow():
-	global walkCount
-
-	window.blit(bg, (0,0))
-	if walkCount + 1 >= 27:
-		walkCount = 0
-	if left:
-		window.blit(walkLeft[walkCount//3], (x, y))
-		walkCount += 1
-	elif right:
-		window.blit(walkRight[walkCount//3], (x, y))
-		walkCount += 1
-	else:
-		window.blit(char, (x, y))
-	pygame.display.update() # refreshed the game
+pygame.display.set_caption('Game')
 
 
+def car(x, y):
+	GameDisplay.blit(CarImg, (x, y))
+
+x = (width * 0.45)
+y = (height * 0.8)
 
 
-run = True
-while run:   #main game loop
-	clock.tick(27)
- 
-	for event in pygame.event.get(): #loops through every event to see if the have or haven't happened
-		if event.type == pygame.QUIT: #if you click the x, run is assigned False
-			run = False # breaks the loop and goes to pygame.quit()
-	
-	keys = pygame.key.get_pressed() # a list of keys
+while not crashed:
 
-	if keys[pygame.K_LEFT] and x > 0:
-		x -= velocity
-		left = True
-		right = False
-	elif keys[pygame.K_RIGHT] and x < SURF_WIDTH - CHAR_WIDTH:
-		x += velocity
-		left = False
-		right = True
-	else:
-		right = False
-		left = False
-		walkCount = 0
-	
-	if not (isJump):  # WHILE NOT JUMPING, DO THIS
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
 
-		if keys[pygame.K_SPACE]:
-			isJump = True
-			right = False
-			left = False
-			walkCount = 0
-	else:  # While jumping, do this
-		if jumpCount >= -10:
-			neg = 1
-			if jumpCount < 0:
-				neg = -1
-			y -= (jumpCount ** 2) * 0.5 * neg
-			jumpCount -= 1
-		else:
-			isJump = False
-			jumpCount = 10
-	
-	redrawGameWindow()
+	GameDisplay.fill(white)
+	car(x, y)
+	pygame.display.update()
+
+	clock.tick(60)
+
 
 pygame.quit()
