b575a6328b739b76b18385fc06c5e4fc488c0920
robert neuzil <robertneuzil@gmail.com>
2018-12-21 21:00:20 -0500

    much easier to read

--- first/game.py
+++ first/game.py
@@ -1,12 +1,20 @@
 import pygame
 pygame.init()
+win = pygame.display.set_mode((500, 480))
+pygame.display.set_caption("Game")
 
-win = pygame.display.set_mode((500,480))
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'),
+             pygame.image.load('R3.png'), pygame.image.load('R4.png'),
+             pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'), pygame.image.load('R8.png'),
+             pygame.image.load('R9.png')]
 
-pygame.display.set_caption("First Game")
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'),
+            pygame.image.load('L3.png'), pygame.image.load('L4.png'),
+            pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), pygame.image.load('L8.png'),
+            pygame.image.load('L9.png')]
 
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 bg = pygame.image.load('bg.jpg')
 char = pygame.image.load('standing.png')
 
@@ -14,44 +22,41 @@ clock = pygame.time.Clock()
 
 
 class player(object):
-    def __init__(self,x,y,width,height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+    def __init__(self):
+        self.x = 200
+        self.y = 410
+        self.width = 64
+        self.height = 64
         self.vel = 5
         self.isJump = False
         self.left = False
         self.right = False
         self.walkCount = 0
         self.jumpCount = 10
-
+        self.run = True
+   
     def draw(self, win):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
 
         if self.left:
-            win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
             self.walkCount += 1
         elif self.right:
-            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
-            self.walkCount +=1
+            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
         else:
-            win.blit(char, (self.x,self.y))
-
+            win.blit(char, (self.x, self.y))
 
 
 def redrawGameWindow():
-    win.blit(bg, (0,0))
+    win.blit(bg, (0, 0))
     man.draw(win)
-
     pygame.display.update()
 
-
-#mainloop
-man = player(200, 410, 64,64)
-run = True
-while run:
+# mainloop
+man = player()
+while man.run:
     clock.tick(27)
 
     for event in pygame.event.get():
