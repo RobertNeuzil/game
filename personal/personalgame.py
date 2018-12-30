da110a0b3b36a2c1d596d0ad2c24eb4f42f7aa8a
robert neuzil <robertneuzil@gmail.com>
2018-12-30 03:48:11 -0500

    class based constructer to create blocks that will become platform to jump on

--- personal/personalgame.py
+++ personal/personalgame.py
@@ -12,10 +12,29 @@ red = (255, 0, 0)
 black = (0, 0, 0)
 
 
+class rects():
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+	def create(self):
+		pygame.draw.rect(window, red, (self.x, self.y, self.width, self.height))
+
+square_one = rects(50, 530, 40, 40)
+square_two = rects(100, 200, 200, 20)
+square_three = rects(100, 300, 150, 20)
+square_four = rects(100, 100, 340, 20)
+square_five = rects(100, 400, 200, 20)
+square_six = rects(60, 300, 150, 20)
 def draw():
 	pygame.time.delay(40)
 	window.fill(black)
-	pygame.draw.rect(window, red, (x, y, width, height))
+	square_one.create()
+	square_two.create()
+	square_three.create()
+	square_four.create()
+	square_five.create()
 	pygame.display.update()
 	
 	
@@ -25,14 +44,15 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 	keys = pygame.key.get_pressed()
-	if keys[pygame.K_LEFT] and x > 0:
-		x-= velocity
-	if keys[pygame.K_RIGHT] and x < 600 - width:
-		x += velocity
-	if keys[pygame.K_DOWN] and y < 600 - height:
-		y += velocity
-	if keys[pygame.K_UP] and y > 0:
-		y -= velocity
+	if keys[pygame.K_LEFT] and square_one.x > 0:
+		square_one.x-= velocity
+	if keys[pygame.K_RIGHT] and square_one.x < 600 - width:
+		square_one.x += velocity
+	if keys[pygame.K_DOWN] and square_one.y < 600 - height:
+		square_one.y += velocity
+	if keys[pygame.K_UP] and square_one.y > 0:
+		square_one.y -= velocity
+	
 	draw()
 	
 pygame.quit()
