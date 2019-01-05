0be41e180ed7a13ac85acb85cfc5c9e958840f75
robert neuzil <robertneuzil@gmail.com>
2019-01-05 16:35:55 -0500

    informative

--- tutorial/sprite_example.py
+++ tutorial/sprite_example.py
@@ -1,54 +1,73 @@
-#pygame template - skeleton for new pygame
+# Pygame sprite Example
 import pygame
 import random
+import os
 
-width = 800
-height = 800
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
 
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up assets folders
+# Windows: "C:\Users\chris\Documents\img"
+# Mac: "/Users/chris/Documents/img"
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
 
 class Player(pygame.sprite.Sprite):
-	# sprite for the player
-	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((50, 50))
-		self.image.fill(green)
-		self.rect = self.image.get_rect()
-		self.rect.center = (width / 2, height / 2)
-	def update(self):
-		self.rect.x += 5
+    # sprite for the Player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
 
-#initialize pygame and create window
+    def update(self):
+        self.rect.x += 5
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+# initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-#Game Loop
+# Game loop
 running = True
 while running:
-	# keep loop running at right speed / same speed
-	clock.tick(FPS)
-	#process input (events)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-	#update
-	all_sprites.update()	
-	
-	#draw / render
-	screen.fill(black)
-	all_sprites.draw(screen)
-	# after drawing everything, flip display
-	pygame.display.flip()
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+    all_sprites.update()
+
+    # Draw / render
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
 
 pygame.quit()
