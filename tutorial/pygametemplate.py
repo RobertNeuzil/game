d0924a44ae8c5cb242106383ca72aa2ae538e721
robert neuzil <robertneuzil@gmail.com>
2019-01-06 07:30:42 -0500

    raining sprites

--- tutorial/pygametemplate.py
+++ tutorial/pygametemplate.py
@@ -4,12 +4,15 @@ import random
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
+
+# define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-# initialize pygame
+
+# initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -20,13 +23,21 @@ all_sprites = pygame.sprite.Group()
 # Game loop
 running = True
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-	# update
-	all_sprites.update()
-	#draw
-	screen.fill(BLACK)
-	all_sprites.draw(screen)
-	# always do last
-	pygame.display.flip()
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+    all_sprites.update()
+
+    # Draw / render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
