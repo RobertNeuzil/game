16991a245dc768f955fb6962865856f38d68ac41
robert neuzil <robertneuzil@gmail.com>
2019-01-06 06:43:05 -0500

    whatever

--- tutorial/sprite_example.py
+++ tutorial/sprite_example.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 
 WIDTH = 800
 HEIGHT = 600
@@ -12,22 +13,32 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assetts
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
 class Player(pygame.sprite.Sprite):
     # sprite for the Player
     def __init__(self):
         # this line is required to properly create the sprite
         pygame.sprite.Sprite.__init__(self)
         # create a plain rectangle for the sprite image
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
+        self.image.set_colorkey(BLACK)
         # find the rectangle that encloses the image
         self.rect = self.image.get_rect()
         # center the sprite on the screen
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
-
+        self.y_speed = 5
     def update(self):
         # any code here will happen every time the game loop updates
         self.rect.x += 5
+        self.rect.y += self.y_speed
+
+        if self.rect.bottom > HEIGHT - 200:
+        	self.y_speed = -5
+        if self.rect.top < 200:
+        	self.y_speed = 5
         if self.rect.left > WIDTH:
             self.rect.right = 0
 
